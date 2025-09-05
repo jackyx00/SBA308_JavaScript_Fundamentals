36855cec8e41f9ddd961499d3d405094a29fec32
Ying Jie Mei <jackymei95@gmail.com>
2025-09-05 16:55:01 -0400

    seperationOfLearners

--- script.js
+++ script.js
@@ -91,6 +91,77 @@ function checkPoints(points) {
   }
 }
 
+let submissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140
+    }
+  }
+];
+
+
+
+function get (submissions, AssignmentGroup) {
+    let newLearners = [];
+    let newSubmissions = submissions;
+    let learnerId;
+    let result = [];
+
+    while (newSubmissions.length > 0) {
+        let currentId = newSubmissions[0].learner_id;
+        for (let i = 0; i < newSubmissions.length; i++) {
+            if (newLearners == []) {
+                newLearners.push(newSubmissions[i]);
+                newSubmissions.splice(0, 1);
+            } else if (currentId == newSubmissions[i].learner_id) {
+                newLearners.push(newSubmissions[i]);
+                newSubmissions.splice(0, 1);
+            } else if (currentId != newSubmissions[i].learner_id) {
+                continue;
+            }
+        }
+        
+    }
+    return newLearners;
+}
+
+console.log(get(submissions, AssignmentGroup));
+
 function getLearnerData(course, ag, submissions) {
   // here, we would process this data to achieve the desired result.
   try {
@@ -112,20 +183,27 @@ function getLearnerData(course, ag, submissions) {
     sameLearnerId[learnerid].push(submissions[i]);
   }
 
-  const result = [
-    {
-      id: 125,
-      avg: 0.985, // (47 + 150) / (50 + 150)
-      1: 0.94, // 47 / 50
-      2: 1.0, // 150 / 150
-    },
-    {
-      id: 132,
-      avg: 0.82, // (39 + 125) / (50 + 150)
-      1: 0.78, // 39 / 50
-      2: 0.833, // late: (140 - 15) / 150
-    },
-  ];
+  for (let i = 0; i < ag.assignments.length; i++) {
+    for (let j = 0; j < sameLearnerId.length; i++) {}
+  }
+  if (ag.assignments) {}
+
+  const result = [];
+
+//   const result = [
+//     {
+//       id: 125,
+//       avg: 0.985, // (47 + 150) / (50 + 150)
+//       1: 0.94, // 47 / 50
+//       2: 1.0, // 150 / 150
+//     },
+//     {
+//       id: 132,
+//       avg: 0.82, // (39 + 125) / (50 + 150)
+//       1: 0.78, // 39 / 50
+//       2: 0.833, // late: (140 - 15) / 150
+//     },
+//   ];
 
   return result;
 }
